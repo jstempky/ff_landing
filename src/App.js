bcc0179d63c5a79b73500964880ded1acb4a4655
Joey Stempky <joeystempky@Joeys-Air-4.attlocal.net>
2019-09-18 23:16:47 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './resources/logo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {email: 'fitfort@email.com'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({email: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Thank you for signing up: ' + this.state.email);
+    event.preventDefault();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>
+            FitFort helps Fitness Professionals/Personal Trainers sustain longer client relationships by making it easier to build personalized fitness plans and helping client’s see their progress.
+          </h3>
+          <form className="container-style">
+            <input className="input-style" placeholder="fitfort@email.com" value={this.state.value} onChange={this.handleChange} type="text" name="email" />
+            <input className="button-style" type="submit" value="Sign up to be a pilot tester!" />
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
