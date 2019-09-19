0c191c3e0ee83942ba1ca9670e4b29226ff92332
Joey Stempky <joeystempky@Joeys-Air-4.attlocal.net>
2019-09-19 13:08:14 -0400

    small tweaks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import logo from './resources/logo.png';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {email: 'fitfort@email.com'};
+    this.state = {email: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -13,12 +14,25 @@ class App extends Component {
 
   handleChange(event) {
     this.setState({email: event.target.value});
+    console.log(this.state)
   }
 
   handleSubmit(event) {
-    alert('Thank you for signing up: ' + this.state.email);
-    event.preventDefault();
+    axios.post('fitfort.herokuapp.com/emailsignup', {
+      email: this.state.email,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
+
 
   render(){
     return (
@@ -29,8 +43,19 @@ class App extends Component {
             FitFort helps Fitness Professionals/Personal Trainers sustain longer client relationships by making it easier to build personalized fitness plans and helping clients see their progress.
           </h3>
           <form className="container-style">
-            <input className="input-style" placeholder="enter your email here!" value={this.state.value} onChange={this.handleChange} type="text" name="email" />
-            <input className="button-style" type="submit" value="Sign up to be a pilot user!" />
+            <input
+              className="input-style"
+              placeholder="enter your email here!"
+              value={this.state.value}
+              onChange={this.handleChange}
+              type="text"
+              name="email"
+            />
+            <input
+              className="button-style"
+              type="submit"
+              value="Sign up to be a pilot user!"
+            />
           </form>
         </header>
       </div>
